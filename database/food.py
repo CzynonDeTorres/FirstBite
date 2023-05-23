57bdd148f0e0290576f4d4133559cdd84e43e43d
Zis <czy094@gmail.com>
2023-05-23 13:32:30 +0800

    removed temp code, added temp search food function

--- database/food.py
+++ database/food.py
@@ -10,19 +10,23 @@ FOOD_DB.row_factory = sqlite3.Row
 
 def add_new_food(**kwargs):
     cur = FOOD_DB.cursor()
+    
     new_row = ''
-
-    for key, value in kwargs.items():
-        print("{} : {}".format(key, value))
-
     for values in kwargs.values():
         new_row += f'"{values}", '
     new_row = new_row[:len(new_row)-2]
 
-    print(new_row)
     sql = (
         "INSERT INTO food (name, serving_size, calories, fat, cholesterol, sodium, carbs, protein, micronutrients)"
         "VALUES "
         f'({new_row});'
     )
     cur.executescript(sql)
+
+def search_food():
+    sql = (
+        "SELECT "
+        "name, serving_size, calories "
+        "FROM food"
+        "WHERE name LIKE '%?%'"
+    )
