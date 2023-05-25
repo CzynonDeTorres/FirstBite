4b4ef50ba5d1667080536f78ad62fd8a79f28e49
Zis <czy094@gmail.com>
2023-05-25 09:08:29 +0800

    moved function in the class

--- resources/food.py
+++ resources/food.py
@@ -10,22 +10,20 @@ class Food():
     name: str
     serving_size: str
     calories: int
-    fat: int
-    cholesterol: int
-    sodium: int
-    carbs: int
-    protein: int
-    micronutrients: str
+    fat: float
+    cholesterol: float
+    sodium: float
+    carbs: float
+    protein: float
 
-def new_food(self, **kwargs):
-    self.name = kwargs.get('name')
-    self.serving_size = kwargs.get('serving_size')
-    self.calories = kwargs.get('calories')
-    self.fat = kwargs.get('fat')
-    self.cholesterol = kwargs.get('cholesterol')
-    self.sodium = kwargs.get('sodium')
-    self.carbs = kwargs.get('carbs')
-    self.protein = kwargs.get('protein')
-    self.micronutrients = kwargs.get('micronutrients', '')
+    def new_food(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.serving_size = kwargs.get('serving_size')
+        self.calories = kwargs.get('calories')
+        self.fat = kwargs.get('fat')
+        self.cholesterol = kwargs.get('cholesterol')
+        self.sodium = kwargs.get('sodium')
+        self.carbs = kwargs.get('carbs')
+        self.protein = kwargs.get('protein')
 
-    print(self)
+        print(self)
