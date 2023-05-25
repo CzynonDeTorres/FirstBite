9cdb95bd5a26cd268a98054a77b71e83e8827dee
Zis <czy094@gmail.com>
2023-05-25 15:06:06 +0800

    added add meal function

--- database/meal.py
+++ database/meal.py
@@ -15,12 +15,27 @@ USER_DB = sqlite3.connect(USER_DB_FILE)
 USER_DB.row_factory = sqlite3.Row
 USER_cur = USER_DB.cursor()
 
-def show_meal():
-    sql = (
-        "SELECT "
-        "name, "
-        "WHERE "
-        "user_id = ?"
-        "AND type = ?"
-        "AND date = ?"
-    )
+class Meal:
+    def show_meal(self):
+        sql = (
+            "SELECT "
+            "name, "
+            "WHERE "
+            "user_id = ?"
+            "AND type = ?"
+            "AND date = ?"
+        )
+
+    def add_meal(self, *args):
+        cur = USER_DB.cursor()
+        new_row = ''
+
+        for values in args:
+            new_row += f'"{values}", '
+        new_row = new_row[:len(new_row)-2]
+        sql = (
+            "INSERT INTO meals (type, user_id, food_id, name, date) "
+            "VALUES "
+            f'({new_row});'
+        )
+        cur.executescript(sql)
